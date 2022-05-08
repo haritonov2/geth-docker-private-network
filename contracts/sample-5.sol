456e91cfdac2926191e60e6bca2ca489c2c9d2b8
sashah <sasha.h@speedandfunction.com>
2022-05-08 14:37:36 -0400

    Init "generate-genesis" allocation population

--- contracts/sample-5.sol
+++
@@ -1,9 +0,0 @@
-contract Sample5 {
-    mapping (int => string) myMap;
-
-    function sample(int key, string val) {
-        myMap[key] = val;
-
-        mapping (int => string) myMap2 = myMap;
-    }
-}
